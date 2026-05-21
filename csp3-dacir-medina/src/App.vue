daef0ed2db6135b71cf282c2d4fdeb15280c3c0f
medinanathaniel28 <medinanathaniel28@gmail.com>
2026-05-21 20:13:09 +0800

    added register page

--- csp3-dacir-medina/src/App.vue
+++ csp3-dacir-medina/src/App.vue
@@ -1,11 +1,38 @@
-<script setup>
+
+<script>
+
+  // import NavbarComponent from './components/NavbarComponent.vue';
+
+  import {useGlobalStore} from './stores/global';
+
+
+  import {onBeforeMount} from 'vue';
+
+
+  export default {
+
+    components: {
+      // NavbarComponent
+    },
+    setup() {
+
+      const {getUserDetails} = useGlobalStore();
+
+      onBeforeMount(() => getUserDetails(localStorage.getItem("token")));
+    }
+  }
 
 </script>
 
 <template>
-  
+
+  <!-- <NavbarComponent /> -->
+
+  <router-view />
+
 </template>
 
 <style scoped>
+
 
 </style>
--- csp3-dacir-medina/src/pages/RegisterPage.vue
+++ csp3-dacir-medina/src/pages/RegisterPage.vue
@@ -12,7 +12,7 @@
     const mobileNum = ref("");
     const email = ref("");
     const password = ref("");
-    const confirmPass = ref("");
+    const confirmPassword = ref("");
     // for button
     const isEnabled = ref(false);
 
@@ -22,9 +22,143 @@
 
     const {user} = useGlobalStore();
 
+    watch([firstName, lastName, mobileNum, email, password, confirmPassword], (currentValue, oldValue) => {
+
+    	// check if everything have value and password and confirm pass is match
+    	if (currentValue.every(input => input !== "") && currentValue[4] === currentValue[5] && currentValue[2].length === 11) {
+    		// allow submit
+    		isEnabled.value = true;
+    	} else {
+    		isEnabled.value = false;
+    	}
+    })
+
+    async function handleSubmit(e) {
+    	e.preventDefault();
+
+    	try {
+    		let response = await api.post('users/register', {
+    			firstName: firstName.value,
+                lastName: lastName.value,
+                email: email.value,
+                mobileNum: mobileNum.value,
+                password: password.value
+    		})
+
+    		if (response.status === 201) {
+    			notyf.success(response.data.message);
+
+    			firstName.value = "";
+                lastName.value = "";
+                mobileNum.value = "";
+                email.value = "";
+                password.value = "";
+                confirmPass.value = "";
+
+                // router.push({path: '/login'}) // uncomment when we have login
+    		} else {
+
+    			console.error(e); // for debugging purposes, remove when we're done
+
+    			notyf.error("Registration Failed. Please contact administrator.");
+    		}
+    	} catch (e) {
+    		if (e.response.status === 404 || e.response.status === 401 || e.response.status === 400 || e.response.status === 409) {
+
+    			console.error(e); // for debugging purposes, remove when we're done
+
+    			notyf.error(e.response.data.message);
+    		} else {
+    			console.error(e); // for debugging purposes, remove when we're done
+
+    			notyf.error("Registration Failed. Please contact administrator.")
+    		}
+    	}
+    }
+
+    /*onBeforeMount(() => {
+        if(user.email){
+            router.push({path: '/products'})
+        }
+    })*/ // uncomment when we have product
+
 
 </script>
 
 <template>
-	
+    <div class="container-fluid">
+        <h1 class="my-5 pt-3 text-muted text-center">Register</h1> 
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-5 border border rounded-3 mx-auto p-5">
+                <form @submit="handleSubmit">
+
+                    <div class="mb-3">
+                        <label for="fName" class="form-label">First Name:</label>
+                        <input 
+                        type="text" 
+                        class="form-control"
+                        id="fName"
+                        placeholder="Enter your First Name" 
+                        v-model="firstName" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="lName" class="form-label">Last Name:</label>
+                        <input 
+                        type="text" 
+                        class="form-control"
+                        id="lName" 
+                        placeholder="Enter your Last Name"
+                        v-model="lastName" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="emailInput" class="form-label">Email Address:</label>
+                        <input 
+                        type="email" 
+                        class="form-control"
+                        id="emailInput" 
+                        placeholder="Enter your email" 
+                        v-model="email" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="mobile" class="form-label">Mobile Number:</label>
+                        <input 
+                        type="text" 
+                        class="form-control"
+                        id="mobile" 
+                        placeholder="Enter your 11 digit mobile number"
+                        v-model="mobileNum" />
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="passwordInput" class="form-label">Password:</label>
+                        <input 
+                        type="password" 
+                        class="form-control"
+                        id="passwordInput" 
+                        placeholder="Enter your password" 
+                        v-model="password" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="cpasswordInput" class="form-label">Confirm Password:</label>
+                        <input 
+                        type="password" 
+                        class="form-control"
+                        id="cpasswordInput" 
+                        placeholder="Verify your password"
+                        v-model="confirmPassword"
+                         />
+                    </div>
+                    <div class="d-grid mt-5">
+                        <!-- Enabled submit button -->
+                        <button type="submit" class="btn btn-primary btn-block" v-if="isEnabled">Submit</button>
+                        <!-- Disabled submit button -->
+                        <button type="submit" class="btn btn-danger btn-block" v-else disabled>Please enter your registration</button>
+                    </div>
+                </form>
+            </div>
+            <div class="text-center mt-3">
+                <p>Already have an account? <a href="">Click here</a> to log in. </p>
+            </div>
+        </div>
+    </div>
 </template>
